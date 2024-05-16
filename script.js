9aeb28074b7c74f551e8dda5063bbe61dccbae08
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-05-16 12:29:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,17 +2,19 @@ function fibonacci(num) {
 // your code here
 	let a = 0;
 	let b = 1;
+	let 
 
 const  arr = [0,1]
 
 for(let i = 0; i<=num; i++){
-  let temp = a+b;
+   temp = a+b;
  arr.push(temp)
   a = b;
   b = temp
+	
 }
 
-return temp;
+return arr[arr.length-1];
 }
 
 module.exports = fibonacci;
